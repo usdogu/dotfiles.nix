dedb982bd392b1e204065c6ca9197db66f6bb36c
Doğu Us <uspro@disroot.org>
2023-05-14 19:07:16 +0300

    add foot config

--- home/dogu/sway.nix
+++ home/dogu/sway.nix
@@ -3,6 +3,7 @@
     enable = true;
     config = {
       bars = [ ];
+      terminal = "foot";
       menu = "${pkgs.bemenu}/bin/bemenu-run";
       modifier = "Mod4";
       input = { "*" = { xkb_layout = "tr"; }; };
@@ -78,5 +79,33 @@
     wrapperFeatures.gtk = true;
   };
 
-  home.packages = with pkgs; [ foot ];
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "Iosevka Nerd Font :pixelsize=15:antialias=true:autohint=true";
+      };
+      colors = {
+        background = "282828";
+        foreground = "ebdbb2";
+        regular0 = "282828";
+        regular1 = "cc241d";
+        regular2 = "98971";
+        regular3 = "d79921";
+        regular4 = "458588";
+        regular5 = "b16286";
+        regular6 = "689d6a";
+        regular7 = "a89984";
+        bright0 = "928374";
+        bright1 = "fb4934";
+        bright2 = "b8bb26";
+        bright3 = "fabd2f";
+        bright4 = "83a598";
+        bright5 = "d3869b";
+        bright6 = "8ec07c";
+        bright7 = "ebdbb2";
+      };
+    };
+  };
+
 }
