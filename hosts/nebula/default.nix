4223883590520b9e498e6e3339706121d7748bd0
Doğu Us <uspro@disroot.org>
2023-07-02 16:41:21 +0300

    encrypt dns

--- hosts/nebula/default.nix
+++ hosts/nebula/default.nix
@@ -1,6 +1,6 @@
 # configuration.nix
 
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   imports = [
@@ -35,6 +35,35 @@
   networking = {
     hostName = "nebula";
     networkmanager.enable = true;
+    networkmanager.dns = "none";
+    nameservers = [ "127.0.0.1" "::1" ];
+  };
+  services.resolved.enable = lib.mkForce false; # just in case
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = false; # enable if you get ipv6 support
+      block_ipv6 = true; # same but disable it
+      require_dnssec = true;
+      require_nolog = true;
+      require_nofilter = true;
+      bootstrap_resolvers = [ "1.1.1.1:53" "8.8.8.8:53" ];
+      server_names = [ "cloudflare" ];
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key =
+          "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
+    };
+  };
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
   };
 
   services.printing = {
