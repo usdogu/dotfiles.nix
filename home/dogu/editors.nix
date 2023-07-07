411196f516a0cc400665040e23371d71524fc827
Doğu Us <uspro@disroot.org>
2023-07-07 13:11:58 +0300

    friendship ended with emacs now helix is my best friend

--- home/dogu/editors.nix
+++ home/dogu/editors.nix
@@ -1,26 +1,6 @@
-{ lib, config, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
-  programs.emacs = {
-    enable = true;
-    extraPackages = epkgs: [ epkgs.vterm ];
-  };
-  services.emacs.enable = true;
-  xdg.configFile."doom" = {
-    source = ./configs/.doom.d;
-    recursive = true;
-  };
-
-  # install doomemacs on activation of generation
-  home.activation = {
-    DoomEmacsAction = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      if [[ ! -d "${config.xdg.configHome}"/emacs ]]; then
-        $DRY_RUN_CMD ${pkgs.git}/bin/git clone $VERBOSE_ARG --depth=1 --single-branch https://github.com/doomemacs/doomemacs.git "${config.xdg.configHome}"/emacs
-        PATH=${pkgs.git}/bin:$PATH EMACS="${config.programs.emacs.finalPackage}"/bin/emacs $DRY_RUN_CMD "${config.xdg.configHome}"/emacs/bin/doom sync
-      fi
-    '';
-  };
-
   programs.helix = {
     enable = true;
     defaultEditor = true;
