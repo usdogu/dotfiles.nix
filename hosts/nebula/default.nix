4a8b6fb1ef90ad47ad2239db03dc66fd365dc173
Doğu Us <uspro@disroot.org>
2023-07-26 13:31:33 +0300

    fonts.fonts -> fonts.packages

--- hosts/nebula/default.nix
+++ hosts/nebula/default.nix
@@ -30,7 +30,8 @@
     timeZone = "Europe/Istanbul";
   };
 
-  fonts.fonts = with pkgs; [ (nerdfonts.override { fonts = [ "Iosevka" ]; }) ];
+  fonts.packages = with pkgs;
+    [ (nerdfonts.override { fonts = [ "Iosevka" ]; }) ];
 
   networking = {
     hostName = "nebula";
