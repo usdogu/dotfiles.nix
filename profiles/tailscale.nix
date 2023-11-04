5c71ad71c0ff5cde7bf64320a85ed5a4b7285e55
Doğu Us <73913444+usdogu@users.noreply.github.com>
2023-11-05 00:28:00 +0300

    use the new tailscale module

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,38 +1,10 @@
-{ inputs, ... }:
-{ pkgs, lib, config, ... }:
+{ config, ... }:
 
 {
-  services.tailscale.enable = true;
-
-  age.secrets.tailscaleAuthKey.file =
-    "${inputs.self}/secrets/tailscaleAuthKey.age";
-
-  systemd.services.tailscaled-autoconnect = {
-    description = "Automatic connection to Tailscale";
-    after = [ "tailscaled.service" ];
-    requires = [ "tailscaled.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-    script = ''
-      # wait for tailscaled to settle
-      sleep 2
-
-      # check if we are already authenticated to tailscale
-      status="$(${
-        lib.getExe config.services.tailscale.package
-      } status -json | ${pkgs.jq}/bin/jq -r .BackendState)"
-      if [ $status = "Running" ]; then # if so, then do nothing
-        exit 0
-      fi
-
-      # otherwise authenticate with tailscale
-      ${config.services.tailscale.package}/bin/tailscale up \
-        --authkey file:${config.age.secrets.tailscaleAuthKey.path} \
-        --ssh \
-        --hostname=${config.networking.hostName}
-    '';
-  };
+  age.secrets.tailscaleAuthKey.file = ../secrets/tailscaleAuthKey.age;
+  
+  services.tailscale = {
+    enable = true;
+    authKeyFile = config.age.secrets.tailscaleAuthKey.path;
+  }
 }
