fddd567e80e46ee2f12a7fb57156ee716b675256
Doğu Us <uspro@disroot.org>
2024-09-30 23:19:45 +0300

    allow reboot on users

--- hosts/nebula/default.nix
+++ hosts/nebula/default.nix
@@ -96,7 +96,25 @@
   };
 
   security = {
-    polkit.enable = true;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+          polkit.addRule(function(action, subject) {
+          if (
+            subject.isInGroup("users")
+              && (
+                action.id == "org.freedesktop.login1.reboot" ||
+                action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+                action.id == "org.freedesktop.login1.power-off" ||
+                action.id == "org.freedesktop.login1.power-off-multiple-sessions"
+              )
+            )
+          {
+            return polkit.Result.YES;
+          }
+        });      
+      '';
+    };
     rtkit.enable = true;
   };
 
