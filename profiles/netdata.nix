503c13a9070e22bb2c412f4208caadce76a83fcb
Doğu Us <uspro@disroot.org>
2024-10-04 22:08:07 +0300

    add netdata config

--- profiles/netdata.nix
+++ profiles/netdata.nix
@@ -6,5 +6,13 @@
     enable = true;
     package = pkgs.netdataCloud;
     claimTokenFile = config.age.secrets.netdataClaimToken.path;
+    config = {
+      global = {
+        history = "604800"; # 1 week
+        "update every" = "5";
+      };
+      logs.level = "error";
+      web.mode = "none";
+    };
   };
 }
